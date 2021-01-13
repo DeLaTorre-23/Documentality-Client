30f93c277fd86fffcef346ae1276de87125f127f
Serch <ztorre23@gmail.com>
2021-01-13 15:38:25 +0100

    [add] implemented PropTypes required to MovieCardView

--- src/components/MovieCardView/MovieCardView.jsx
+++ src/components/MovieCardView/MovieCardView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Card, Button} from 'react-bootstrap'
 
 export class MovieCardView extends React.Component {
@@ -13,17 +14,25 @@ export class MovieCardView extends React.Component {
       <Card style={{ width: '22rem' }} className="movieCard mb-3">
         <Card.Img variant="top" src={documentary.ImagePath} />
         <Card.Body>
-          <Card.Title>{documentary.Title /*+ ' - ' + documentary.Released*/}</Card.Title>
+          {/*<Card.Title>{documentary.Title + ' - ' + documentary.Released}</Card.Title>*/}
           {/*<Card.Text>{documentary.Description}</Card.Text>*/}
           <Button  
             onClick={() => onClick(documentary)}
             variant="link"
-            className="expandDocumentary"
+            className="expandDocumentary MovieCardView"
           >  
-            Details
+            {documentary.Title}
           </Button> 
         </Card.Body>
       </Card>            
     );
   }
 }
+
+MovieCardView.propTypes = {
+  documentary: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
