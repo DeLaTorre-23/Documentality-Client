3245dafc5052b7b51d6d79308d1c98f61d7a5721
serch <ztorre23@mail.com>
2021-01-24 13:50:14 +0100

    added images to the project

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -51689,7 +51689,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59496" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62835" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
