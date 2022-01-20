e8bcfe11e1453532fecffde512756d246211d6b0
Sergio De La Torre <ztorre23@gmail.com>
2022-01-20 01:26:51 +0100

    Updated babel in package.json

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,26 +1,18 @@
-export const SET_DOCUMENTARIES = 'SET_DOCUMENTARIES';
-export const SET_FILTER = 'SET_FILTER';
+export const SET_DOCUMENTARIES = "SET_DOCUMENTARIES";
+export const SET_FILTER = "SET_FILTER";
 
 // We can put the name that we want in "value". value = data
-{/*
-export const editTodo = (index, text) => ({
-  type: EDIT_TODO,
-  index,
-  text
-})
-*/}
-
 export function setDocumentaries(value) {
-  console.log('SET_MOVIES action triggered');
-  return { 
-    type: SET_DOCUMENTARIES, 
-    value
+  console.log("SET_MOVIES action triggered");
+  return {
+    type: SET_DOCUMENTARIES,
+    value,
   };
 }
 
 export function setFilter(value) {
-  return { 
-    type : SET_FILTER, 
-    value
+  return {
+    type: SET_FILTER,
+    value,
   };
 }
